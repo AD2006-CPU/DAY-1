d47880fd6f80bf4a545a14226f44b43d44ad4423
AD2006-CPU <anushkadey424@gmail.com>
2025-03-13 21:46:53 +0530

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    char a;
+    scanf("%c", &a);
+    printf("Enter a character:");
+    printf("%c=%d",a,a);
+    return 0;
+}
+
---
+++ addition of two integers.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    int a=10;
+    int b;
+    b=30;
+    printf("%d %d %d",a,b,a+b);
+    return 0;
+}
